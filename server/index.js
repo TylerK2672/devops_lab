8557bcfab94a68034ae967829f21f88fa84a8037
Tyler Kendall <tylerchristiankendall@gmail.com>
2022-06-30 15:28:12 -0600

    error added

--- server/index.js
+++ server/index.js
@@ -5,10 +5,30 @@ const app = express();
 
 const SERVER_PORT = process.env.PORT || 3005;
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: 'b57323fdf1e941cfbc58ccb980414c40',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 app.get('/', (req, res) =>
 {
     res.sendFile(path.join(__dirname, '../index.html'));
-});
+})
+
+app.get('/err', (req, res) =>
+{
+    try {
+        badFunction();
+    } catch (error) {
+        console.log('ya messed up')
+    }
+})
 
 app.listen(SERVER_PORT, () =>
 {
